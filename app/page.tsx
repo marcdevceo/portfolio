51419a2ab6e65db47b3847be610a0c5bf8c5d1bf
marcdevceo <marcdevceo@icloud.com>
2025-02-05 23:32:24 -0500

    added a footer and corrected some typos

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,51 @@
-import Image from "next/image";
+import Card from "@/components/Card";
+import { LampContainer } from "@/components/Lamp";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main>
+      <header>
+        <div className="text-center text-5xl font-bold">
+          <LampContainer children={
+            <h1>MarcDevCEO's Portfolio</h1>
+          } />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+        <div className="m-14 text-center text-2xl italic">
+          <h2>Welcome to my Developing World!!!</h2>
+        </div>
+      </header>
+      <section className="flex flex-col items-center my-14">
+          <p>🚀 Building the Future, One Line of Code at a Time</p>
+          <p>🎯 Check out my latest web apps, insights, and experience.</p>
+          <p>👉 Check out my projects, dive into my insights, and see how I bring ideas to life!</p>
+      </section>      
+      <section>
+        <h1 className="text-2xl font-semibold text-center my-5">
+          Click to Check Out each Collection
+        </h1>
+        <div className="flex flex-wrap justify-evenly gap-5 m-5">
+          <Card
+            image="/images/projects.jpg"
+            content="My Collection of All my Projects"
+            href="/projects"
+            alt="My Projects"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+          <Card
+            image="/images/blog3.jpg"
+            content="A Collection of My Thoughts through My Journey"
+            href="/blog"
+            alt="My Blog"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          <Card
+            image="/images/resume.jpg"
+            content="A Collection of My Skills and Experiences"
+            href="/resume"
+            alt="My Resume"
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        </div>
+      </section>
+    </main>
   );
 }
---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <main>
+        <footer className='text-sm py-8 text-center'>
+            &copy; Created by MarcDevCEO aka Marcus Kimber
+        </footer>
+    </main>
+  )
+}
+
+export default Footer
